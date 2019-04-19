336e93985a63ae24c93052a9f1648c93922687b1
Amine GAHBICHE <aga@connecthings.com>
2019-04-19 11:25:11 +0200

    comment header file imports

--- zip/all/CocoaLumberjack.framework/Headers/CocoaLumberjack.h
+++ zip/all/CocoaLumberjack.framework/Headers/CocoaLumberjack.h
@@ -69,7 +69,7 @@ FOUNDATION_EXPORT const unsigned char CocoaLumberjackVersionString[];
 #ifndef DD_LEGACY_MACROS
     #define DD_LEGACY_MACROS 0
 #endif
-
+/*
 // Core
 #import <CocoaLumberjack/DDLog.h>
 
@@ -103,4 +103,4 @@ FOUNDATION_EXPORT const unsigned char CocoaLumberjackVersionString[];
 #import <CocoaLumberjack/DDLegacyMacros.h>
 
 #import <CocoaLumberjack/SwiftLogLevel.h>
-
+*/
